2a34b77f1713d409454469f13254fb9e9d476d5a
Simon Neutert <simon@Simons-MBP.fritz.box>
2019-01-23 20:33:34 +0100

    fridgy init

---
+++ src/components/Products.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid mt-4">
+    <h1 class="h1">Product</h1>
+    <div v-for="product in products" :key="product.id">
+      <p>{{product.id}} {{product.name}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'products',
+  data () {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Simon'
+        }
+      ]
+    }
+  }
+}
+</script>
