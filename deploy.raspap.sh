1b860c9682dac763138056b4b6428f405bd8fb9b
Simon Neutert <simon@Simons-MBP.fritz.box>
2019-03-09 23:47:00 +0100

    order of params

--- deploy.raspap.sh
+++ deploy.raspap.sh
@@ -2,5 +2,5 @@
 
 git checkout master
 git pull origin master
-docker-compose -f docker-compose.raspap.yml up -d --build
+docker-compose -f docker-compose.raspap.yml up --build -d
 docker-compose -f docker-compose.raspap.yml restart web
--- deploy.rpi.sh
+++ deploy.rpi.sh
@@ -2,5 +2,5 @@
 
 git checkout master
 git pull origin master
-docker-compose -f docker-compose.rpi.yml up -d --build
+docker-compose -f docker-compose.rpi.yml up --build -d
 docker-compose -f docker-compose.rpi.yml restart web
