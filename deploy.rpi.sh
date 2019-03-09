3a19d3c6ee13546d430bc80fd28b6fb19120e887
Simon Neutert <simon@Simons-MBP.fritz.box>
2019-03-09 23:43:34 +0100

    rebuild for deploy

--- deploy.rpi.sh
+++ deploy.rpi.sh
@@ -2,5 +2,6 @@
 
 git checkout master
 git pull origin master
-docker-compose -f docker-compose.rpi.yml up -d web
+docker-compose -f docker-compose.rpi.yml -d web
+docker-compose -f docker-compose.rpi.yml up -d web --build
 docker-compose -f docker-compose.rpi.yml restart web
