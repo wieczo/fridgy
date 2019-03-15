76011f1a11df62e3dc5fa38f2281d3379a3f9e87
Simon Neutert <simonneutert@users.noreply.github.com>
2019-03-15 23:17:55 +0100

    login via RFID (#12)
    * docker it all!

--- rfid-reader/Read.py
+++ rfid-reader/Read.py
@@ -12,9 +12,12 @@ reader = SimpleMFRC522.SimpleMFRC522()
 
 while continue_reading: 
     id, text = reader.read()
-    url = "http://10.3.141.1:8081/user/" + str(id)
-    print(url)
-    r = requests.post(url)
-    print(r.content)
-    time.sleep(5)
+    if id:
+        url = "http://10.3.141.1:8081/user/" + str(id)
+        print(url)
+        r = requests.post(url)
+        print(r.content)
+        time.sleep(5)
+    else:
+        time.sleep(1)
     
